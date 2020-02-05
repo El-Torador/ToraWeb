0a958f72666dee1a0fee531b31d0da6c2cc6c481
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-05 01:07:49 +0100

    the entry point of apps

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
