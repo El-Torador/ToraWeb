e24c2dbf654584bc616a494d2196d1210f3de3e7
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-11 12:14:34 +0100

    the file who store the constaints of apps

---
+++ src/Constants/index.js
@@ -0,0 +1,11 @@
+export  const END_POINT = "/learnship/api/v1.0.0";
+export const config = {
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        "content-type": "application/json"
+    }
+}
+
+export const API_KEY_USERS='LEARNSHIP2020R2767687678686877BVHJDVBHHSUSER'
+
+export const API_KEY_INSTANCE = 'LEARNSHIP2020R27676877Y68YFSDVSBDVSDVBVHJDVB'
