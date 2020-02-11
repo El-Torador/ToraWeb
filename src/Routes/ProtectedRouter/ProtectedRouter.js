3a41fe391a367ab492b07f7e712999ba9171f297
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-11 12:19:12 +0100

    the secure router finalyse

---
+++ src/Routes/ProtectedRouter/ProtectedRouter.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+const ProtectedRouter = ({component: Component, ...rest}) =>{
+    return (
+        <Route {...rest} render={
+            props =>{
+                if(window.sessionStorage.getItem('ssid_learn')){
+                    return <Component {...props} />
+                }else{
+                    return <Redirect to={
+                       {
+                            pathname: "/",
+                            state: {
+                                from: props.location
+                            }
+                       }
+                    } 
+
+                    />
+                }
+            }
+        } 
+
+        />
+            
+    )
+}
+
+export default ProtectedRouter
