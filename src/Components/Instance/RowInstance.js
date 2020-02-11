d0aea1d726a7bb4fb609fae3e41cf6fd3156d12e
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-11 12:11:29 +0100

    update propreties and add the condition and margin

--- src/Components/Instance/RowInstance.js
+++ src/Components/Instance/RowInstance.js
@@ -5,48 +5,52 @@ import Fade from 'react-reveal/Fade'
 import PropTypes from 'prop-types'
 const RowInstance = (props) =>{
     
-        return (
-          <FilterResults
-            value={props.entries}
-            data={props.instance}
-            renderResults={result => {
-              return result.map(item => (
-                <Fade right key={item.id}>
-                  <div className="two wide" key={item.id}>
-                    <div className="ui card raised link">
-                      <div className="content">
-                        <div className="header">{item.name}</div>
-                        <div className="meta">
-                          {item.place}, {item.address}
+        if(props.instance){
+          return (
+            <FilterResults
+              value={props.entries}
+              data={props.instance}
+              renderResults={result => {
+                return result.map(item => (
+                  <Fade right key={item.id}>
+                    <div className="two wide add-margin" key={item.id}>
+                      <div className="ui card raised link">
+                        <div className="content">
+                          <div className="header">{item.name}</div>
+                          <div className="meta">
+                            {item.city}, {item.address}
+                          </div>
+                          <div className="description">
+                            {item.name} situé dans la ville de {item.city} donc
+                          le chef de centre est M. <b>{item.responsable}</b>.
                         </div>
-                        <div className="description">
-                          {item.name} situé dans la ville de {item.place} donc
-                          le chef de centre est <b>{item.responsable}</b>.
+                          <div className="footer">
+                            Contact: {item.phone_number[0]}{" "}
+                            {item.phone_number[1] && "- " + item.phone_number[1]}
+                          </div>
                         </div>
-                        <div className="footer">
-                          Contact: {item.phone_number[0]}{" "}
-                          {item.phone_number[1] && "- " + item.phone_number[1]}
+                        <div className="extra content">
+                          <span className="left floated like">
+                            <Link title="Editer" to={"/instance/edit/" + item.id}>
+                              <i className="icon pencil yellow large"></i>
+                            </Link>
+                          </span>
+                          <span className="right floated star">
+                            <Link title="Voir" to={"/instance/show/" + item.id}>
+                              <i className="icon eye blue large"></i>
+                            </Link>
+                          </span>
                         </div>
-                      </div>
-                      <div className="extra content">
-                        <span className="left floated like">
-                          <Link title="Editer" to={"/instance/edit/" + item.id}>
-                            <i className="icon edit yellow large"></i>
-                          </Link>
-                        </span>
-                        <span className="right floated star">
-                          <Link title="Voir" to={"/instance/show/" + item.id}>
-                            <i className="icon eye blue large"></i>
-                          </Link>
-                        </span>
                       </div>
                     </div>
-                  </div>
-                </Fade>
-              ));
-            }}
-          />
-        );
+                  </Fade>
+                ));
+              }}
+            />
+          );
+        }else{
+          return <p className="description">Pas d'instance pour le moment !</p>
+        }
     
 }
 
