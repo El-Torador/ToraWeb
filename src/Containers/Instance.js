3b352faa0759d4d0d2dc04a2ef0e43d89fbf0a20
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-12 16:20:28 +0100

    add jsdoc and simplify the code of Component

--- src/Containers/Instance.js
+++ src/Containers/Instance.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { toast } from 'react-toastify'
 import Head from '../Components/Header/Header'
 import RowInstance from '../Components/Instance/RowInstance'
-import { Loader, Modal, Icon, Button, Header } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { getInstance, addInstance } from "../Controllers/instances/CRUD_instance";
-import {
-  DateInput
-} from "semantic-ui-calendar-react";
-
+import ModalLogout from '../Components/Sections/ModalLogout'
+import AddInstance from '../Components/Instance/AddInstance'
+import { Loader } from 'semantic-ui-react'
+import { getInstance } from "../Controllers/instances/CRUD_instance";
 import './Instance.css'
+
+/**
+ * INSTANCE CONTAINER
+ */
 class Instance extends Component {
     constructor(props){
         super(props)
@@ -34,12 +35,12 @@ class Instance extends Component {
                       });
                 })
             }else{
-                toast.warn(`Pas d'instance pour le moment.`, {position: 'bottom-left', hideProgressBar: true})
+              toast.warn(`❌Pas d'instance pour le moment.`, {position: 'bottom-left', hideProgressBar: true})
             }
         })
         .catch((err)=>{
             console.log(err)
-              this.setState({ isLoading: false });
+          this.setState({ isLoading: false }, () => toast.error('❌'+err.message, {position: 'bottom-left', hideProgressBar: true}));
         })
         
     }
@@ -51,64 +52,8 @@ class Instance extends Component {
         const {value} = event.target
         this.setState({value})
     }
-    /**
-     * PERMET DE METTREA JOUR LA DATE 
-     * @param { Event } e
-     */
-     handleDate = (e, {name, value}) => {
-       if (this.state.hasOwnProperty(name)) {
-         this.setState({ [name]: value });
-       }
-    }
-    /**
-     * ENREGISTRER UNE INSTANCE
-     * @param { Event } e
-     */
-    handlePostInstance = (e) =>{
-      e.preventDefault()
-      const instance = {
-        name: e.target[0].value,
-        city: e.target[1].value,
-        address: e.target[2].value,
-        responsable: e.target[3].value,
-        phone_number: e.target[4].value.split('-'),
-        inauguration: this.state.date
-      }
-      if(instance.name){
-        if(instance.city){
-          if(instance.address){
-            if(instance.responsable){
-              if(instance.phone_number){
-                if(instance.inauguration){
-                  addInstance(instance)
-                  .then((message)=>{
-                    const i = [...this.state.instance, instance]
-                    this.setState({instance: i}, ()=>{
-                      toast.success('✔️'+message, {position: 'bottom-left', hideProgressBar: true})
-                    })
-                  })
-                  .catch((err)=>{
-                    console.log(err)
-                  })
-                }else{
-                  toast.error('❌La date d\'inauguration est requise.', { position: 'bottom-left', hideProgressBar: true })
-                }
-              }else{
-                toast.error('❌ Le numéro de téléphone est requis.', { position: 'bottom-left', hideProgressBar: true })
-              }
-            }else{
-              toast.error('❌ Le nom du responsable est requis.', { position: 'bottom-left', hideProgressBar: true })
-            }
-          }else{
-            toast.error('❌ L\'adresse est requise.', { position: 'bottom-left', hideProgressBar: true })
-          }
-        }else{
-          toast.error('❌ La ville est requise.', { position: 'bottom-left', hideProgressBar: true })
-        }
-      }else{
-        toast.error('❌ Le nom de l\'instance est requis.', { position: 'bottom-left', hideProgressBar: true })
-      }
-    }
+    
+    
     /**
      * PERMET D'OUVRIR LA MODAL
      */
@@ -164,100 +109,8 @@ class Instance extends Component {
                 >
                   <i className="icon plus"></i>
                 </button>
-                <Modal
-                  dimmer="blurring"
-                  closeIcon
-                  onClose={this.exit}
-                  closeOnDimmerClick={false}
-                  open={this.state.addInstance}
-                  size="large"
-                >
-                  <Header icon="plus" content="Ajouter une instance" />
-                  <Modal.Content>
-                      <form className="ui form" onSubmit={this.handlePostInstance}>
-                        <div className="field">
-                          <div className="ui left icon input">
-                            <i className="icon globe"></i>
-                            <input type="text" name="name" id="instance_name" placeholder="Nom" title="Nom" autoComplete="off" required/>
-                          </div>
-                        </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon home"></i>
-                          <input type="text" name="city" id="instance_city" placeholder="Ville" title="Ville" autoComplete="off" required/>
-                        </div>
-                      </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon map pin"></i>
-                          <input type="text" name="address" id="instance_address" placeholder="Adresse" title="Adresse" autoComplete="off" required/>
-                        </div>
-                      </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon user secret"></i>
-                          <input type="text" name="responsable" id="instance_responsable" placeholder="Responsable ou Chef de centre" title="Responsable ou Chef de centre" autoComplete="off" required/>
-                        </div>
-                      </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon phone"></i>
-                          <input type="text" name="phone_number" id="instance_phone_numbers" placeholder="Téléphone" title="Téléphone" autoComplete="off" required />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <DateInput
-                          closable={true}
-                          name="date"
-                          placeholder="Date d'inauguration"
-                          title="Date d'inauguration"
-                          value={this.state.date}
-                          iconPosition="left"
-                          onChange={this.handleDate}
-                          required
-                        />
-                      </div>
-                      <Button color="yellow" inverted type="submit">
-                          <Icon name="checkmark" /> Ajouter
-                      </Button>
-                      <Button
-                        inverted
-                        color="red"
-                        onClick={this.exit}
-                      >
-                        <Icon name="remove" /> Annuler
-                      </Button>
-                        
-                      </form>
-                  </Modal.Content>
-                </Modal>
-                <Modal
-                  open={this.state.modalOpen}
-                  onClose={this.handleClose}
-                  basic
-                  closeOnDimmerClick={false}
-                  size="small"
-                >
-                  <Header icon="log out" content="Déconnection" />
-                  <Modal.Content>
-                    <h3>Voulez-vous vraiment vous déconnecter ?</h3>
-                  </Modal.Content>
-                  <Modal.Actions>
-                    <Button
-                      basic
-                      color="yellow"
-                      onClick={this.handleClose}
-                      inverted
-                    >
-                      <Icon name="remove" /> Non
-                    </Button>
-                    <Link to="/">
-                      <Button color="red" inverted>
-                        <Icon name="checkmark" /> Oui
-                      </Button>
-                    </Link>
-                  </Modal.Actions>
-                </Modal>
+                <ModalLogout modalOpen={this.state.modalOpen} onClose={this.handleClose} />
+                <AddInstance date={this.state.date} addInstance={this.state.addInstance} exit={this.exit} />
               </div>
             );
         }
