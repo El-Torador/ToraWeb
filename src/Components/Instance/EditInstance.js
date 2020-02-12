74e6c5600aef7b8d8836a469fd070387aa1dda06
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-12 23:57:47 +0100

    update handleSubmit method and add the condition

--- src/Components/Instance/EditInstance.js
+++ src/Components/Instance/EditInstance.js
@@ -62,26 +62,15 @@ class EditInstance extends Component {
     handleSubmit = (e) => {
       e.preventDefault()
       console.log(e.target)
-      let newInstance = null
-      if(this.state.instance.phone_number.length > 1){
-         newInstance = {
-          name: e.target[0].value,
+      
+      const newInstance = {
+          name: e.target[0].value.toUpperCase(),
           city: e.target[1].value,
           address: e.target[2].value,
           responsable: e.target[3].value,
-          phone_number: [e.target[4].value, e.target[5].value],
+          phone_number: e.target[4].value.split('-'),
           inauguration: this.state.date
         }
-      }else{
-        newInstance = {
-          name: e.target[0].value,
-          city: e.target[1].value,
-          address: e.target[2].value,
-          responsable: e.target[3].value,
-          phone_number: [e.target[4].value],
-          inauguration: this.state.date
-        }
-      }
       editInstance(newInstance, this.instaceId)
       .then((message)=>{
         window.localStorage.setItem('flash', message)
@@ -102,128 +91,152 @@ class EditInstance extends Component {
               <Loader active={true} />
             </div>
         }else{
-            return (
-              <div>
-                <Head location="/instance" handleOpen={this.handleOpen} />
-                <div className="ui container">
-                  <br />
-                  <br />
-                  <h2 className="ui title">
-                    <i className="icon edit large"></i> Editer une instance
-                  </h2>
-                  <form className="ui form large" onSubmit={this.handleSubmit}>
-                    <div className="ui raised segment">
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon globe"></i>
-                          <input
-                            type="text"
-                            name="name"
-                            minLength="4"
-                            autoCapitalize
-                            autoFocus
-                            placeholder="Nom"
-                            title="Nom"
-                            defaultValue={instance.name}
-                            autoComplete="off"
+            if(instance.name && instance.city && instance.address && instance.responsable && instance.phone_number){
+              return (
+                <div>
+                  <Head location="/instance" handleOpen={this.handleOpen} />
+                  <div className="ui container">
+                    <br />
+                    <br />
+                    <h2 className="ui title">
+                      <i className="icon edit large"></i> Editer une instance
+                    </h2>
+                    <form
+                      className="ui form large"
+                      onSubmit={this.handleSubmit}
+                    >
+                      <div className="ui raised segment">
+                        <div className="field">
+                          <div className="ui left icon input">
+                            <i className="icon globe"></i>
+                            <input
+                              type="text"
+                              name="name"
+                              pattern="^IAI-+[a-zA-Z]{2,}"
+                              autoFocus
+                              placeholder="Nom"
+                              title="Nom"
+                              defaultValue={instance.name}
+                              autoComplete="off"
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="field">
+                          <div className="ui left icon input">
+                            <i className="icon home"></i>
+                            <input
+                              type="text"
+                              name="city"
+                              minLength="4"
+                              placeholder="Ville"
+                              title="Ville"
+                              defaultValue={instance.city}
+                              autoComplete="off"
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="field">
+                          <div className="ui left icon input">
+                            <i className="icon map pin"></i>
+                            <input
+                              type="text"
+                              minLength="3"
+                              name="address"
+                              placeholder="Adresse"
+                              title="Adresse"
+                              defaultValue={instance.address}
+                              autoComplete="off"
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="field">
+                          <div className="ui left icon input">
+                            <i className="icon user secret"></i>
+                            <input
+                              type="text"
+                              minLength="4"
+                              name="chief"
+                              placeholder="Responsable ou Chef de centre"
+                              title="Responsable ou Chef de centre"
+                              defaultValue={instance.responsable}
+                              autoComplete="off"
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="field">
+                          <div className="ui left icon input">
+                            <i className="icon phone"></i>
+                            <input
+                              type="tel"
+                              pattern="^6+[0-9_-]{8,}" 
+                              name="phone_number"
+                              placeholder="Téléphone"
+                              title="Téléphone"
+                              defaultValue={instance.phone_number && instance.phone_number.join("-")}
+                              autoComplete="off"
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="field">
+                          <DateInput
+                            closable={true}
+                            name="date"
+                            placeholder="Date d'inauguration"
+                            title="Date d'inauguration"
+                            value={this.state.date}
+                            iconPosition="left"
+                            onChange={this.handleDate}
                             required
                           />
                         </div>
-                      </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon home"></i>
-                          <input
-                            type="text"
-                            name="city"
-                            minLength="4"
-                            placeholder="Ville"
-                            title="Ville"
-                            defaultValue={instance.city}
-                            autoComplete="off"
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon map pin"></i>
-                          <input
-                            type="text"
-                            minLength="3"
-                            name="address"
-                            placeholder="Adresse"
-                            title="Adresse"
-                            defaultValue={instance.address}
-                            autoComplete="off"
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon user secret"></i>
-                          <input
-                            type="text"
-                            minLength="4"
-                            name="chief"
-                            placeholder="Responsable ou Chef de centre"
-                            title="Responsable ou Chef de centre"
-                            defaultValue={instance.responsable}
-                            autoComplete="off"
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <div className="ui left icon input">
-                          <i className="icon phone"></i>
-                          <input
-                            type="tel"
-                            pattern="[0-9]{9}"
-                            name="phone_number"
-                            placeholder="Téléphone"
-                            title="Téléphone"
-                            defaultValue={instance.phone_number.join('-')}
-                            autoComplete="off"
-                            required
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <DateInput
-                          closable={true}
-                          name="date"
-                          placeholder="Date d'inauguration"
-                          title="Date d'inauguration"
-                          value={this.state.date}
-                          iconPosition="left"
-                          onChange={this.handleDate}
-                          required
-                        />
-                      </div>
-                      <span className="left float">
-                        <Button color="yellow" inverted>
-                          <Icon name="checkmark" /> Editer
-                        </Button>
-                      </span>
-                      <span className="right float">
-                        <Link to="/instance">
-                          <Button
-                            color="red"
-                            onClick={this.handleClose}
-                            inverted
-                          >
-                            <Icon name="remove" /> Annuler
+                        <span className="left float">
+                          <Button color="yellow" inverted>
+                            <Icon name="checkmark" /> Editer
                           </Button>
-                        </Link>
-                      </span>
-                    </div>
-                  </form>
+                        </span>
+                        <span className="right float">
+                          <Link to="/instance">
+                            <Button
+                              color="red"
+                              onClick={this.handleClose}
+                              inverted
+                            >
+                              <Icon name="remove" /> Annuler
+                            </Button>
+                          </Link>
+                        </span>
+                      </div>
+                    </form>
+                  </div>
+                  <ModalLogout
+                    modalOpen={this.state.modalOpen}
+                    onClose={this.handleClose}
+                  />
                 </div>
-                <ModalLogout modalOpen={this.state.modalOpen} onClose={this.handleClose} />
-              </div>
-            );
+              );
+            }else{
+              return (
+                <div>
+                  <Head location="/instance" handleOpen={this.handleOpen} />
+                  <br />
+                  <div className="ui container">
+                    <br />
+                    <Link to="/instance" className="ui button red inverted">
+                      Retour
+                    </Link>
+                    <br />
+                    <h1 style={{ textAlign: "center" }}>
+                      La ressource n'existe pas !
+                    </h1>
+                  </div>
+                </div>
+              );
+            }
         }
     }
 }
