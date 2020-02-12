463a49b25123f7cbe20fe71985f5b27f07c91e36
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-12 23:55:58 +0100

    update State and postInstance method

--- src/Components/Instance/AddInstance.js
+++ src/Components/Instance/AddInstance.js
@@ -15,7 +15,8 @@ class AddInstance extends Component{
     constructor(props){
         super(props)
         this.state = {
-            date: this.props.date
+            date: null,
+            isLoading: false
         }
     }
 
@@ -35,14 +36,20 @@ class AddInstance extends Component{
      */
     postInstance = (e) => {
         e.preventDefault()
+        this.setState({isLoading: true})
+        let date = this.state.date.split('-')
+        const inauguration = `${date[2]}/${date[1]}/${date[0]}`
         const instance = {
-            name: e.target[0].value,
+            name: e.target[0].value.toUpperCase(),
             city: e.target[1].value,
             address: e.target[2].value,
             responsable: e.target[3].value,
             phone_number: e.target[4].value.split('-'),
-            inauguration: this.state.date
+            inauguration
         }
+        console.log(instance)
+        setTimeout(()=>{
+            
         if (instance.name) {
             if (instance.city) {
                 if (instance.address) {
@@ -52,11 +59,16 @@ class AddInstance extends Component{
                                 addInstance(instance)
                                     .then((message) => {
                                         const i = [...this.state.instance, instance]
-                                        this.setState({ instance: i }, () => {
+                                        this.setState({ instance: i, isLoading: false }, () => {
+                                            this.props.exit()
                                             toast.success('✔️' + message, { position: 'bottom-left', hideProgressBar: true })
                                         })
                                     })
                                     .catch((err) => {
+                                        this.setState({isLoading: false}, ()=>{
+                                            this.props.exit()
+                                            err.message && toast.error('❌ ' + err.message, { position: 'bottom-left', hideProgressBar: true })
+                                        })
                                         console.log(err)
                                     })
                             } else {
@@ -77,6 +89,7 @@ class AddInstance extends Component{
         } else {
             toast.error('❌ Le nom de l\'instance est requis.', { position: 'bottom-left', hideProgressBar: true })
         }
+        }, 1000)
     }
 
     render(){
@@ -95,31 +108,31 @@ class AddInstance extends Component{
                         <div className="field">
                             <div className="ui left icon input">
                                 <i className="icon globe"></i>
-                                <input type="text" minLength="4" autoCapitalize autoFocus name="name" id="instance_name" placeholder="Nom ex:IAI-CENTRE" title="Nom" autoComplete="off" required />
+                                <input type="text" pattern="^IAI-+[a-zA-Z]{2,}" autoFocus name="name" id="instance_name" placeholder="Nom ex:IAI-CENTRE" title="Nom" autoComplete="off" required />
                             </div>
                         </div>
                         <div className="field">
                             <div className="ui left icon input">
                                 <i className="icon home"></i>
-                                <input type="text" name="city" minLength="4" id="instance_city" placeholder="Ville ex:Yaoundé" title="Ville" autoComplete="off" required />
+                                <input type="text" name="city" autoCapitalize="true" id="instance_city" placeholder="Ville ex:Yaoundé" title="Ville" autoComplete="off" required />
                             </div>
                         </div>
                         <div className="field">
                             <div className="ui left icon input">
                                 <i className="icon map pin"></i>
-                                <input type="text" name="address" minLength="3" id="instance_address" placeholder="Adresse ex:Nkolanga'a" title="Adresse" autoComplete="off" required />
+                                <input type="text" name="address" autoCapitalize="true" id="instance_address" placeholder="Adresse ex:Nkolanga'a" title="Adresse" autoComplete="off" required />
                             </div>
                         </div>
                         <div className="field">
                             <div className="ui left icon input">
                                 <i className="icon user secret"></i>
-                                <input type="text" minLength="4" name="responsable" id="instance_responsable" placeholder="Responsable ou Chef de centre ex:ARMAND CLAUDE ABANDA" title="Responsable ou Chef de centre" autoComplete="off" required />
+                                <input type="text" name="responsable" autoCapitalize="true" id="instance_responsable" placeholder="Responsable ou Chef de centre ex:ARMAND CLAUDE ABANDA" title="Responsable ou Chef de centre" autoComplete="off" required />
                             </div>
                         </div>
                         <div className="field">
                             <div className="ui left icon input">
                                 <i className="icon phone"></i>
-                                <input type="tel" pattern="[0-9]{9}-" name="phone_number" id="instance_phone_numbers" placeholder="Téléphone ex:694532343-675664433" title="Téléphone" autoComplete="off" required />
+                                <input type="tel" pattern="^6+[0-9_-]{8,}" name="phone_number" id="instance_phone_numbers" placeholder="Téléphone ex:694532343-675664433" title="Téléphone" autoComplete="off" required />
                             </div>
                         </div>
                         <div className="field">
@@ -134,7 +147,7 @@ class AddInstance extends Component{
                                 required
                             />
                         </div>
-                        <Button color="yellow" inverted type="submit">
+                        <Button color="yellow" inverted type="submit" loading={this.state.isLoading} disabled={this.state.isLoading}>
                             <Icon name="checkmark" /> Ajouter
                       </Button>
                         <Button
@@ -153,7 +166,6 @@ class AddInstance extends Component{
 }
 
 AddInstance.propTypes = {
-    date: PropTypes.string.isRequired,
     exit: PropTypes.func.isRequired,
     addInstance: PropTypes.bool.isRequired
 }
