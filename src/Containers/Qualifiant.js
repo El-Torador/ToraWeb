16e98c887b454aa0edd6aa0cbc2c82142adcd75d
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-12 16:21:46 +0100

    init Qualifiant Component

---
+++ src/Containers/Qualifiant.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Loader } from 'semantic-ui-react'
+import Head from '../Components/Header/Header'
+class Qualifiant extends Component {
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            isLoading: false
+        }
+    }
+
+    render(){
+       if(this.state.isLoading){
+           return <div>
+                <Head localtion="/formation" handleOpen={this.handleOpen} />
+                <Loader active ={true} />
+           </div>
+       }else{
+           return <div>
+               <Head location='/formation' handleOpen={this.handleOpen} />
+               <br />
+               <br />
+               <div className="ui container">
+                   <h1>
+                        <i className="icon wpforms large"></i> Modules des formations qualifiantes
+                   </h1>
+                   
+               </div>
+           </div>
+       }
+    }
+}
+
+export default Qualifiant
