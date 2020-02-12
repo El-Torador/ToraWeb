feec1e646890e1426bbda85a98b0228c5cb110e7
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-12 16:16:34 +0100

    Add comment(jsdoc)

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -4,17 +4,26 @@ import Login from '../Containers/Login'
 import User from '../Containers/User'
 import Instance from '../Containers/Instance'
 import EditInstance from '../Components/Instance/EditInstance'
+import Training from '../Containers/Training'
+import Certifiant from '../Containers/Certifiant'
+import Qualifiant from '../Containers/Qualifiant'
+
+/**
+ * ROUTER COMPONENT
+ */
 class AppRouter extends Component{
     render(){
-        return(
-            <Switch>
-                <Route exact path="/" component={Login}/>
-                <Route path="/users" component={User} />
-                <Route path="/instance/edit/:id" component={EditInstance} />
-                <Route path="/instance" component={Instance} />
-                
-            </Switch>
-        )
+        return (
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/instance/edit/:id" component={EditInstance} />
+            <Route path="/instance" component={Instance} />
+            <Route path="/users" component={User} />
+            <Route path="/formation/certifiante" component={Certifiant} />
+            <Route path="/formation/qualifiante" component={Qualifiant} />
+            <Route path="/formation" component={Training} />
+          </Switch>
+        );
     }
 }
 export default AppRouter
