74002d31a57245b7ccfd599c9a0f0c42d4927742
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-16 20:41:28 +0100

    Add modalLogout and update design

--- src/Containers/Qualifiant.js
+++ src/Containers/Qualifiant.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Loader } from 'semantic-ui-react'
 import Head from '../Components/Header/Header'
-
+import ModalLogout from '../Components/Sections/ModalLogout'
+import './Qualifiant.css'
 /**
  * QUALIFIANT CONTAINER
  */
@@ -10,28 +11,42 @@ class Qualifiant extends Component {
     {
         super(props)
         this.state = {
-            isLoading: false
+            isLoading: false,
+            modalOpen: false
         }
     }
 
+    /**
+     * PERMET D'OUVRIR OU DE FERMER LA MODAL DE DECONNEXION
+     */
+    toggleModal = () => this.setState({ modalOpen: !this.state.modalOpen })
+
     render(){
        if(this.state.isLoading){
-           return <div>
-                <Head localtion="/formation" handleOpen={this.handleOpen} />
-                <Loader active ={true} />
-           </div>
+           return (
+           <div>
+               <Head location="/formation" handleOpen={this.toggleModal} />
+               <div className="ui container padding">
+                   <h1>
+                       <i className="icon wpforms large"></i> Modules des formations qualifiantes
+                   </h1>
+                   <Loader active={true} />
+               </div>
+                   <ModalLogout modalOpen={this.state.modalOpen} onClose={this.toggleModal} />
+           </div>)
        }else{
-           return <div>
-               <Head location='/formation' handleOpen={this.handleOpen} />
+           return (
+           <div>
+               <Head location='/formation' handleOpen={this.toggleModal} />
                <br />
                <br />
-               <div className="ui container">
+               <div className="ui container padding">
                    <h1>
                         <i className="icon wpforms large"></i> Modules des formations qualifiantes
                    </h1>
-                   
                </div>
-           </div>
+               <ModalLogout modalOpen={this.state.modalOpen} onClose={this.toggleModal} />
+           </div>)
        }
     }
 }
