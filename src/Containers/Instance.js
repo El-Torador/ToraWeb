21c4769d473bb9acba1acbb68b9994432991e38b
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-16 20:23:10 +0100

    End update and feature complete checked

--- src/Containers/Instance.js
+++ src/Containers/Instance.js
@@ -7,6 +7,7 @@ import AddInstance from '../Components/Instance/AddInstance'
 import { Loader } from 'semantic-ui-react'
 import { getInstance } from "../Controllers/instances/CRUD_instance";
 import './Instance.css'
+import * as types from '../Constants/index'
 
 /**
  * INSTANCE CONTAINER
@@ -15,7 +16,7 @@ class Instance extends Component {
     constructor(props){
         super(props)
         this.state = {
-            instance: [],
+            instance: types.instance,
             value: '',
             isLoading: true,
             modalOpen: false,
@@ -30,7 +31,10 @@ class Instance extends Component {
                 this.setState({instance}, ()=>{
                       this.setState({ isLoading: false }, ()=>{
                         if(window.localStorage.getItem('flash')){
-                          toast.success('✔️'+window.localStorage.getItem('flash'), {position: 'bottom-left', hideProgressBar: true, onClose: ()=>window.localStorage.clear()})
+                          toast.success('✔️'+window.localStorage.getItem('flash'), {position: 'bottom-left', hideProgressBar: true, onClose: ()=>{
+                            window.localStorage.clear()
+                            window.localStorage.setItem('init_lauch', true)
+                          }})
                         }
                       });
                 })
@@ -39,7 +43,6 @@ class Instance extends Component {
             }
         })
         .catch((err)=>{
-            console.log(err)
           this.setState({ isLoading: false }, () => toast.error('❌'+err.message, {position: 'bottom-left', hideProgressBar: true}));
         })
         
@@ -52,7 +55,23 @@ class Instance extends Component {
         const {value} = event.target
         this.setState({value})
     }
-    
+
+    /**
+     * AJOUTE UNE NOUVELLE INSTANCE
+     */
+    newInstance = instance =>{
+      getInstance()
+      .then((instances)=>{
+        const ins = instances.filter((item)=> item.name === instance.name)
+        instance.id = ins[0].id
+        const i = [...this.state.instance, instance]
+        this.setState({ instance: i })
+      })
+        .catch((err) => {
+          this.setState({ isLoading: false }, () => toast.error('❌' + err.message, { position: 'bottom-left', hideProgressBar: true }));
+        })
+      
+    }
     
     /**
      * PERMET D'OUVRIR OU DE FERMER LA MODAL DE DECONNEXION
@@ -67,7 +86,32 @@ class Instance extends Component {
             return (
               <div>
                 <Head location="/instance" handleOpen={this.toggleModal} />
-                <Loader active={true} />
+                <div className="ui container padding">
+                  <h1>
+                    <i className="icon globe large"></i>Gestion des Instances
+                  </h1>
+                  <br /> <br />
+                  <div className="ui search">
+                    <div className="ui left icon input">
+                      <i className="icon search"></i>
+                      <input
+                        className="prompt"
+                        type="text"
+                        placeholder="Rechercher une instance..."
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                  </div>
+                  <Loader active={true} />
+                </div>
+                <button
+                  className="circular ui icon disabled massive button is_fix"
+                  title="Ajouter une instance"
+                  onClick={this.toggleModalAddInstance}
+                >
+                  <i className="icon plus"></i>
+                </button>
+                <ModalLogout modalOpen={this.state.modalOpen} onClose={this.toggleModal} />
               </div>
             );
         }else{
@@ -108,7 +152,7 @@ class Instance extends Component {
                   <i className="icon plus"></i>
                 </button>
                 <ModalLogout modalOpen={this.state.modalOpen} onClose={this.toggleModal} />
-                <AddInstance addInstance={this.state.addInstance} exit={this.toggleModalAddInstance} />
+                <AddInstance addInstance={this.state.addInstance} exit={this.toggleModalAddInstance} newInstance={this.newInstance} />
               </div>
             );
         }
