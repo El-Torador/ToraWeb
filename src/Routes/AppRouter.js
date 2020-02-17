7d9b677083dca0f5cc92b3db209cdbe574608d68
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-17 12:01:27 +0100

    Parsing path in english

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -7,6 +7,9 @@ import EditInstance from '../Components/Instance/EditInstance'
 import Training from '../Containers/Training'
 import Certifiant from '../Containers/Certifiant'
 import Qualifiant from '../Containers/Qualifiant'
+import Learner from '../Containers/Learner'
+import Former from '../Containers/Former'
+import EditLearner from '../Components/Learner/EditLearner'
 
 /**
  * ROUTER COMPONENT
@@ -16,12 +19,16 @@ class AppRouter extends Component{
         return (
           <Switch>
             <Route exact path="/" component={Login} />
+            <Route path="/learner/edit/:id" component={EditLearner} />
             <Route path="/instance/edit/:id" component={EditInstance} />
             <Route path="/instance" component={Instance} />
             <Route path="/users" component={User} />
-            <Route path="/formation/certifiante" component={Certifiant} />
-            <Route path="/formation/qualifiante" component={Qualifiant} />
-            <Route path="/formation" component={Training} />
+            <Route path="/training/certifying" component={Certifiant} />
+            <Route path="/training/qualifying" component={Qualifiant} />
+            <Route path="/training" component={Training} />
+            <Route path="/learner" component={Learner} />
+            <Route path="/former" component={Former} />
+            
           </Switch>
         );
     }
