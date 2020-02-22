be4185f497d7678cc450fc78477032fe1e616ec8
Jordan Kagmeni <kagmeni77@gmail.com>
2020-02-22 21:05:50 +0100

    Add E-Learning, EditModule and Discipline

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -11,6 +11,10 @@ import Learner from '../Containers/Learner'
 import Former from '../Containers/Former'
 import EditLearner from '../Components/Learner/EditLearner'
 import Statistic from '../Containers/Statistic'
+import E_Learning from '../Containers/E-Learning'
+import Discipline from '../Containers/Discipline'
+import EditDiscipline from '../Components/Discipline/EditDiscipline'
+import EditModule from '../Components/Qualifiant/EditQualifiant'
 /**
  * ROUTER COMPONENT
  */
@@ -19,12 +23,16 @@ class AppRouter extends Component{
         return (
           <Switch>
             <Route exact path="/" component={Login} />
+            <Route path="/training/qualifying/module/discipline/edit/:id" component={EditDiscipline} />
+            <Route path="/training/qualifying/module/discipline/:id" component={Discipline} />
+            <Route path="/training/qualifying/module/edit/:id" component={EditModule} />
             <Route path="/learner/edit/:id" component={EditLearner} />
             <Route path="/instance/edit/:id" component={EditInstance} />
             <Route path="/instance" component={Instance} />
             <Route path="/users" component={User} />
             <Route path="/training/certifying" component={Certifiant} />
-            <Route path="/training/qualifying" component={Qualifiant} />
+            <Route path="/training/qualifying/module" component={Qualifiant} />
+            <Route path="/training/e-learning" component={E_Learning} />
             <Route path="/training" component={Training} />
             <Route path="/learner" component={Learner} />
             <Route path="/former" component={Former} />
