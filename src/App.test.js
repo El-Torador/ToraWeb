1e41e9d7c79d21d0c4b6aa5c1967651f41b204ad
Jordan Kagmeni <kagmeni77@gmail.com>
2020-04-07 13:51:16 +0200

    update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title IAI-Learnship', () => {
+  const { getByText } = render(<Router><App /></Router>);
+  const element = getByText(/IAI-Learnship/i);
+  expect(element).toBeInTheDocument();
 });
